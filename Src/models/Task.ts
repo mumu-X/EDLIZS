78dd54331d21d7cadc9435da9728ef99b2f0f019
Munyaradzi Rutsate <munyaradzirutsate@Munyaradzis-Mac-mini.local>
2024-10-29 01:06:23 +0200

    2

--- Src/models/Task.ts
+++ Src/models/Task.ts
@@ -6,13 +6,13 @@ import { BSON, ObjectSchema, Realm } from "realm";
 export class Task extends Realm.Object<Task> {
     _id!: BSON.ObjectId;
     name!: string;
-  
+
     static schema: ObjectSchema = {
-      name: 'Task',
-      properties: {
-        _id: 'objectId',
-        name: {type: 'string', indexed: 'full-text'},
-      },
-      primaryKey: '_id',
+        name: 'Task',
+        properties: {
+            _id: 'objectId',
+            name: { type: 'string', indexed: 'full-text' },
+        },
+        primaryKey: '_id',
     };
-  }
+}
